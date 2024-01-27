28e5cdaae8a12797113c3c10d37950766da7db0d
Felix Prasanna <fpx@mit.edu>
2024-01-27 02:12:12 -0500

    impl of block formatter (fuzzy)
    TODO: don't mess with macro input

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,45 @@
+use std::{fs, path::Path};
+
 use anyhow::Context;
+use cargo_unfmt::{BlockUnformatter, Tokens, Unformat};
+use walkdir::WalkDir;
 
 fn main() -> anyhow::Result<()> {
     let src = include_str!("../long-rust-file.rs");
+    let tokens = Tokens::tokenize(src);
 
-    let parsed = syn::parse_file(src).context("failed to parse file")?;
+    let formatted = tokens.format();
+    fs::write("formatted.rs", formatted).context("failed to write to file")?;
 
-    // for item in parsed.items {
-    //     println!("{item:?}")
-    // }
+    unfmt()
+}
 
+fn unfmt() -> anyhow::Result<()> {
+    let input = Path::new("/Users/fpx/code/rust/cargo-unfmt/test_crates/input/");
+    let output = Path::new("/Users/fpx/code/rust/cargo-unfmt/test_crates/output/");
+    fs::create_dir_all(output).context("failed to create output directory")?;
+
+    for file in WalkDir::new(input) {
+        let file = file.context("failed to walkdir file")?;
+        if file.file_type().is_dir() || file.path().to_str().unwrap().contains(".git/") {
+            continue;
+        }
+
+        let relative = file.path().strip_prefix(input).unwrap();
+        let path = output.join(relative);
+        fs::create_dir_all(path.parent().unwrap())
+            .context("failed to create output subdirectory")?;
+        fs::File::create(&path).context("failed to create new file")?;
+
+        if path.extension().is_some_and(|ext| ext == "rs") {
+            let src =
+                String::from_utf8(fs::read(file.path()).context("failed to read source file")?)
+                    .context("file was not utf-8")?;
+            let formatted = BlockUnformatter::<80>.unformat(Tokens::tokenize(&src).tokens());
+            fs::write(&path, &formatted).context("failed to write formatted source over")?;
+        } else {
+            fs::copy(file.path(), &path).context("failed to copy file over")?;
+        }
+    }
     Ok(())
 }
